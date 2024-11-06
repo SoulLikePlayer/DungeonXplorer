904c479e62fb9ce942270229ea345ae320282382
Louis Lazare <lazare231@etu.unicaen.fr>
2024-11-06 10:50:39 +0100

    [FEAT] : Modification du css pour la présentation de l'histoire

--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -4,7 +4,8 @@
             <!--Contenaire pour l'histoire, visualisation du hero et création du hero -->
         </div>
         <div class="container-items">
-            <!--Contenaire pour l'histoire, la connexion et deconnexion -->
+            <h2 id="HistoireTitre">"Les Ombres du Val Perdu"</h2>
+            <p>Dans le village du Val Perdu, des jeunes filles disparaissent dans la forêt maudite. Le bourgmestre vous supplie de retrouver sa fille disparue. Au cœur des ombres se dresse un château en ruines, où un sorcier redouté tisse son intrigue. Oserez-vous choisir votre chemin à travers des créatures terrifiantes et des énigmes anciennes ? Plongez dans "Au Val Perdu" et découvrez si vous avez le courage de percer les mystères des ténèbres !</p>
         </div>
         <div class="container-items">
             <!--dernier contenaire pour je sais pas quoi encore -->
