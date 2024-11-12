c1df7c5fb70a735098b8a977e5381f6081abb69b
Louis Lazare <lazare231@etu.unicaen.fr>
2024-11-12 07:36:04 +0100

    [FIX] : Suppressions de la partie 'Aventure' de la navbare

--- app/views/layout/navbar.php
+++ app/views/layout/navbar.php
@@ -10,9 +10,7 @@
                 <li class="nav-item">
                     <a class="nav-link" href="/DungeonXplorer">Accueil</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/DungeonXplorer/adventure">Aventures</a>
-                </li>
+
                 <li class="nav-item">
                     <a class="nav-link" href="/DungeonXplorer/about">À propos</a>
                 </li>
