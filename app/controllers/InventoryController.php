80c2c09d7583bbeb8fa5990a85893eeb32fa9f26
Nathan CHIRAC <chiracnathan59@gmail.com>
2024-11-13 10:38:28 +0100

    fix inventaire debut

--- app/controllers/InventoryController.php
+++ app/controllers/InventoryController.php
@@ -3,13 +3,18 @@
 class InventoryController extends Controller {
 
     public function test(){
+        $this->view('users/inventory');
+
         $username = $_POST['username'] ?? '';
         $password = $_POST['password'] ?? '';
         $email = $_POST['email'] ?? '';
 
         $test = new Inventory();
         $test->getInventory($username, $password, $email);
-        echo($test);
+        $_SESSION['inventaire'] = $test;
+
+        $this->view('users/inventorytest');
     }
 
 }
+?>
